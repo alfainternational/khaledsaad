3007a787c1975ce92844c7f8528b92c42dc12709
Khaled Saad <khaledaasaaad@gmail.com>
2026-07-28 09:32:28 +0300

    chore: publish Android 1.0.3 metadata

--- config/mobile.php
+++ config/mobile.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'version' => env('MOBILE_APP_VERSION', '1.0.2'),
-    'build' => (int) env('MOBILE_APP_BUILD', 3),
+    'version' => env('MOBILE_APP_VERSION', '1.0.3'),
+    'build' => (int) env('MOBILE_APP_BUILD', 4),
     'android_package' => 'net.khaledsaad.ksgrowth_mobile',
     'ios_bundle' => 'net.khaledsaad.ksgrowthMobile',
     'apk_path' => public_path('downloads/khaledsaad-growth.apk'),
